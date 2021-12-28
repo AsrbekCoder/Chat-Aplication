d9550e8e63babec83f653441ad23b4c6799e452a
Asrbek <87761511+AsrbekCoder@users.noreply.github.com>
2021-12-28 22:05:59 +0500

    chat design done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="f902943a-3100-446d-8f11-33499e875740"
+      userName="AsrbekCoder"
+      userSecret="123123"
+    />
+  );
+};
+
+export default App;
